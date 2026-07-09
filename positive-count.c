7ec54744a40de2424bf0623adee7535303e1dcde
Subasri S <subasrividhya2008@gmail.com>
2026-07-09 13:56:25 +0530

    positive count

---
+++ positive-count.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+int i,n,count=0;
+scanf("%d",&n);
+int a[n];
+for(i=0;i<n;i++){
+scanf("%d",&a[i]);
+}
+for(i=0;i<n; i++){
+if(a[i]>0)
+ count++;
+}
+
+if(count==0){
+printf("No positive numbers\n");
+}
+else{
+printf("%d",count);
+}
+return 0;
+}
