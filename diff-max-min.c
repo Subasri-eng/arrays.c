4297219e69b76146aeb537c757f9647dc4fae154
Subasri S <subasrividhya2008@gmail.com>
2026-07-09 14:18:59 +0530

    difference between max and min

---
+++ diff-max-min.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int i,n;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    int max=a[0],min=a[0];
+    for(i=0;i<n;i++){
+        if(a[i]>max){
+            max=a[i];
+        }
+        if(a[i]<min){
+            min=a[i];
+        }
+    }
+    printf("%d %d\n",max,min);
+    printf("%d\n",max-min);
+    
+}
