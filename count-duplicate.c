874cadc7a56f19cae609009e4df0cd129d1b3163
Subasri S <subasrividhya2008@gmail.com>
2026-07-09 23:02:06 +0530

    To count  duplicate numbers

---
+++ count-duplicate.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main() {
+    int i, j, n, count = 0;
+
+    scanf("%d", &n);
+    int a[n];
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    for(i = 0; i < n; i++) {
+        for(j = i + 1; j < n; j++) {
+            if(a[i] == a[j]) {
+                count++;
+                break;
+            }
+        }
+    }
+    if(count == 0) {
+        printf("No duplicate found");
+    }
+    else {
+        printf("%d", count);
+    }
+    return 0;
+}
