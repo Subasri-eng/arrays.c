847f9f0b6e39aba8524da4543ea18d49cd3f4c2f
Subasri S <subasrividhya2008@gmail.com>
2026-07-11 14:37:20 +0530

    To Find The Middle Elements In The Array

---
+++ mid-num.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main() {
+    int i, n;
+
+    scanf("%d", &n);
+
+    int a[n];
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    if(n % 2 == 0)
+        printf("%d %d", a[n/2 - 1], a[n/2]);
+    else
+        printf("%d", a[n/2]);
+
+    return 0;
+}
