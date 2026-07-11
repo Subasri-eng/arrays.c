55675fb0700beed46a8b2b4cc3d5670a0d78cca4
Subasri S <subasrividhya2008@gmail.com>
2026-07-11 07:07:03 +0530

    To  Remove the Negative Elements From The Array

---
+++ rem-neg.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int i,n,j=0;
+    scanf("%d",&n);
+    int a[n],b[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++){
+        if(a[i]>=0){
+            b[j]=a[i];
+            j++;
+        }
+        
+    }
+    for(i=0;i<j;i++)
+        printf("%d",b[i]);
+    
+}
